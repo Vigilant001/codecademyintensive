9f3fb4938eb0bac8c29392baca415a76323ff4cd
James Christianson <jdc1144@Jamess-MBP.attlocal.net>
2018-12-04 17:07:23 -0800

    updated 12 4

--- js/game-logic.js
+++ js/game-logic.js
@@ -3,10 +3,10 @@
 // what values come in from the entry on page...
 const P1 = 'Player One';
 const P2 = 'Player Two';
-const Rock = 'rock';
-const Paper = 'paper';
-const Scissors = 'scissors';
-const Tie = 'tie';
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
+const tie = 'tie';
 
 // Player One Move Types and Values
 let playerOneMoveOneType;
@@ -26,8 +26,23 @@ let playerTwoMoveThreeValue;
 
 // How do we subtract from the 99 strengths and assign them to one of the values from the start?
 
+const validMoveTypes = (v1, v2, v3) => validTypes(v1) && validTypes(v2) && validTypes(v3);
+
+const validMoveValues = (t1, t2, t3) => t1 >= 1 && t2 >=1 && t3 >= 1 && t1 + t2 + t3 >= 99;
+
+const validType = (type) => type === 'rock' || type === 'paper' || type === 'scissors';
+
+
 // Set the Player Moves variables (using inputs from page?)
 function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+
+    if (!validMoveTypes(moveOneType, moveTwoType, MoveThreeType)) {
+        return;
+    }
+
+    if (!validMoveValues(moveOneValue, moveTwoValue, MoveThreeValue)) {
+        return;
+    }
 
     switch (player) {
         case P1:
@@ -50,81 +65,81 @@ function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoV
     }
 }
 
+
+
 // Find the round winner
 function getRoundWinner(roundNumber) {
     //compares both player's move types and values for that round, and returns the appropriate winner
 
     // global move value as not less than 1? 
-    if (playerOneMoveOneValue || playerOneMoveTwoValue || playerOneMoveThreeValue || playerTwoMoveOneValue || playerTwoMoveTwoValue || playerTwoMoveThreeValue === 0) {
-        return null;
+    if (playerOneMoveOneValue >= 1 || playerOneMoveTwoValue >= 1 || playerOneMoveThreeValue >= 1 || playerTwoMoveOneValue  >= 1 || playerTwoMoveTwoValue >= 1 || playerTwoMoveThreeValue >= 1) {
+        return;
     }
 // round 1
 
-    if (playerOneMoveOneType === 'rock' && playerTwoMoveOneType === 'scissors' || playerOneMoveOneType === 'paper' && playerTwoMoveOneType === 'rock' || playerOneMoveOneType === 'scissors' && playerTwoMoveOneType === 'paper' ) {
-        return 'Player One';
-    } else if (playerTwoMoveOneType === 'rock' && playerOneMoveOneType === 'scissors' || playerTwoMoveOneType === 'paper' && playerOneMoveOneType === 'rock' || playerTwoMoveOneType === 'scissors' && playerOneMoveOneType === 'paper' ){
-        return 'Player Two';
-    } else if (playerOneMoveOneType === playerTwoMoveOneType) {
-        return 'Its a Tie!';
-    }
+    if (roundNumber = 1) {
+        if (playerOneMoveOneType === 'rock' && playerTwoMoveOneType === 'scissors' || playerOneMoveOneType === 'paper' && playerTwoMoveOneType === 'rock' || playerOneMoveOneType === 'scissors' && playerTwoMoveOneType === 'paper' ) {
+          return 'Player One';
+        } else if (playerTwoMoveOneType === 'rock' && playerOneMoveOneType === 'scissors' || playerTwoMoveOneType === 'paper' && playerOneMoveOneType === 'rock' || playerTwoMoveOneType === 'scissors' && playerOneMoveOneType === 'paper' ){
+          return 'Player Two';
+        } else if (playerOneMoveOneType === playerTwoMoveOneType) {
         
-    if (playerOneMoveOneValue > playerTwoMoveOneValue) {
-        return 'Player One';
-    } else if (playerOneMoveOneValue < playerTwoMoveOneValue) {
-        return 'Player Two';
-    } else if (playerOneMoveOneValue === playerTwoMoveOneValue) {
-        return 'Its a Tie!';
+            if (playerOneMoveOneValue > playerTwoMoveOneValue) {
+                return 'Player One';
+            } else if (playerOneMoveOneValue < playerTwoMoveOneValue) {
+                return 'Player Two';
+            } else if (playerOneMoveOneValue === playerTwoMoveOneValue) {
+                return 'Tie';
+            }
+        }
     }
 
 // round 2 
 
-    if (playerOneMoveTwoType === 'rock' && playerTwoMoveTwoType === 'scissors' || playerOneMoveTwoType === 'paper' && playerTwoMoveTwoType === 'rock' || playerOneMoveTwoType === 'scissors' && playerTwoMoveTwoType === 'paper' ) {
-        return 'Player One';
-    } else if (playerTwoMoveTwoType === 'rock' && playerOneMoveTwoType === 'scissors' || playerTwoMoveTwoType === 'paper' && playerOneMoveTwoType === 'rock' || playerTwoMoveTwoType === 'scissors' && playerOneMoveTwoType === 'paper' ){
-        return 'Player Two';
-    } else if (playerOneMoveTwoType === playerTwoMoveTwoType) {
-        return 'Its a Tie!';
-    }
-        
-    if (playerOneMoveTwoValue > playerTwoMoveTwoValue) {
-        return 'Player One';
-    } else if (playerOneMoveTwoValue < playerTwoMoveTwoValue) {
-        return 'Player Two';
-    } else if (playerOneMoveTwoValue === playerTwoMoveTwoValue) {
-        return 'Its a Tie!';
+    if (roundNumber = 2) {
+        if (playerOneMoveTwoType === 'rock' && playerTwoMoveTwoType === 'scissors' || playerOneMoveTwoType === 'paper' && playerTwoMoveTwoType === 'rock' || playerOneMoveTwoType === 'scissors' && playerTwoMoveTwoType === 'paper' ) {
+            return 'Player One';
+        } else if (playerTwoMoveTwoType === 'rock' && playerOneMoveTwoType === 'scissors' || playerTwoMoveTwoType === 'paper' && playerOneMoveTwoType === 'rock' || playerTwoMoveTwoType === 'scissors' && playerOneMoveTwoType === 'paper' ){
+            return 'Player Two';
+        } else if (playerOneMoveTwoType === playerTwoMoveTwoType) {
+
+            if (playerOneMoveTwoValue > playerTwoMoveTwoValue) {
+                return 'Player One';
+            } else if (playerOneMoveTwoValue < playerTwoMoveTwoValue) {
+                return 'Player Two';
+            } else if (playerOneMoveTwoValue === playerTwoMoveTwoValue) {
+                return 'Tie';
+            }
+        }
     }
 
 // round 3
 
+if (roundNumber = 3) {
     if (playerOneMoveThreeType === 'rock' && playerTwoMoveThreeType === 'scissors' || playerOneMoveThreeType === 'paper' && playerTwoMoveThreeType === 'rock' || playerOneMoveThreeType === 'scissors' && playerTwoMoveThreeType === 'paper' ) {
         return 'Player One';
     } else if (playerTwoMoveThreeType === 'rock' && playerOneMoveThreeType === 'scissors' || playerTwoMoveThreeType === 'paper' && playerOneMoveThreeType === 'rock' || playerTwoMoveThreeType === 'scissors' && playerOneMoveThreeType === 'paper' ){
         return 'Player Two';
     } else if (playerOneMoveThreeType === playerTwoMoveThreeType) {
-        return 'Its a Tie!';
+
+        if (playerOneMoveThreeValue > playerTwoMoveThreeValue) {
+            return 'Player One';
+        } else if (playerOneMoveThreeValue < playerTwoMoveThreeValue) {
+            return 'Player Two';
+        } else if (playerOneMoveThreeValue === playerTwoMoveThreeValue) {
+            return 'Tie';
+        }
     }
-        
-    if (playerOneMoveThreeValue > playerTwoMoveThreeValue) {
-        return 'Player One';
-    } else if (playerOneMoveThreeValue < playerTwoMoveThreeValue) {
-        return 'Player Two';
-    } else if (playerOneMoveThreeValue === playerTwoMoveThreeValue) {
-        return 'Its a Tie!';
-    }
-  }
+}
 
 // need a function for total rounds won? set to player totals?
 
-// Find the game winner
-function getGameWinner() {
 
-    if (playerOneTotals > playerTwoTotals) {
-        return 'Player One Wins';
-    } else if (playerOneTotals < playerTwoTotals) {
-        return 'Player Two Wins';
-    } else if (playerOneTotals === playerTwoTotals) {
-        return 'Its a Tie!';
-    }
+function getGameWinner() {
+    // Find the game winner
+    
+
+
 }
 
 
@@ -132,13 +147,13 @@ function getGameWinner() {
 // Bonus section for computer player randoms
 
 
+let compMove1;
+let compMove2;
+let compMove3;
+
 function setComputerMoves() {
 
     // still needs to add the values between 1 and 99 distributed between all three moves...
-
-    let compMove1;
-    let compMove2;
-    let compMove3;
 
     const randomMove1 = Math.floor(Math.random()* 3);
     const randomMove2 = Math.floor(Math.random()* 3);
